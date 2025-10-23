d22d641047221ed089ce9d25acce2a74964542e3
wedo <costadefelipe@gmail.com>
2025-10-22 22:44:39 -0300

    Add the omitempty directive

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -8,6 +8,6 @@ type Movie struct {
 	Title     string    `json:"title"`
 	Year      int32     `json:"year,omitempty"`
 	Runtime   int32     `json:"runtime,omitempty"`
-	Genres    []string  `json:"genres",omitempty`
+	Genres    []string  `json:"genres,omitempty"`
 	Version   int32     `json:"version"`
 }
